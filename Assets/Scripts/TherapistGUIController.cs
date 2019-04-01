eb1c04fe5ed5b02927572f3a1d6d4a51363c2d2f
AshleyXIV <ashleyxiv@gmail.com>
2019-04-01 18:57:11 -0400

    Scripts for Therapist Menu

---
+++ Assets/Scripts/TherapistGUIController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class TherapistGUIController : MonoBehaviour
+{
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void buttonClickedHome()
+    {
+		SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+	
+	public void buttonClickedOffice()
+    {
+		SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+	
+	public void buttonClickedSurvey()
+    {
+		SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+	
+}
