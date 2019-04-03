6343bb1d429b641ed0f5fd8058396f23904e13da
Matt Castleberry <13787641+mootcastle@users.noreply.github.com>
2019-04-02 23:08:24 -0400

    Add files via upload

---
+++ MyHand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyHand : MonoBehaviour
+{
+    public OVRInput.Controller c;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<LightButton>() != null)
+        {
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, c) >= 0.75f)
+            {
+                if (other.GetComponent<LightButton>().lightsAreOn) other.GetComponent<LightButton>().lightsAreOn = false;
+                else other.GetComponent<LightButton>().lightsAreOn = true;
+            }
+                
+        }
+    }
+}
